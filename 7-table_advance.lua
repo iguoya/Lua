bba7e6db1d6eb719f05c78167e590cb60fdb8a67
雪虎 <375478250@qq.com>
2021-04-01 14:10:38 +0800

    文件读写和协程

--- 7-table_advance.lua
+++ 7-table_advance.lua
@@ -20,3 +20,78 @@ for key,value in ipairs(array)
 do
    print(key, value)
 end
+
+
+-- 简单的 table
+mytable = {}
+print("mytable 的类型是 ",type(mytable))
+
+mytable[1]= "Lua"
+mytable["wow"] = "修改前"
+print("mytable 索引为 1 的元素是 ", mytable[1])
+print("mytable 索引为 wow 的元素是 ", mytable["wow"])
+
+-- alternatetable和mytable的是指同一个 table
+alternatetable = mytable
+
+print("alternatetable 索引为 1 的元素是 ", alternatetable[1])
+print("mytable 索引为 wow 的元素是 ", alternatetable["wow"])
+
+alternatetable["wow"] = "修改后"
+
+print("mytable 索引为 wow 的元素是 ", mytable["wow"])
+
+-- 释放变量
+alternatetable = nil
+print("alternatetable 是 ", alternatetable)
+
+-- mytable 仍然可以访问
+print("mytable 索引为 wow 的元素是 ", mytable["wow"])
+
+mytable = nil
+print("mytable 是 ", mytable)
+
+
+fruits = {"banana","orange","apple"}
+-- 返回 table 连接后的字符串
+print("连接后的字符串 ",table.concat(fruits))
+
+-- 指定连接字符
+print("连接后的字符串 ",table.concat(fruits,", "))
+
+-- 指定索引来连接 table
+print("连接后的字符串 ",table.concat(fruits,", ", 2,3))
+
+
+fruits = {"banana","orange","apple"}
+
+-- 在末尾插入
+table.insert(fruits,"mango")
+print("索引为 4 的元素为 ",fruits[4])
+
+-- 在索引为 2 的键处插入
+table.insert(fruits,2,"grapes")
+print("索引为 2 的元素为 ",fruits[2])
+
+print("最后一个元素为 ",fruits[5])
+table.remove(fruits)
+print("移除后最后一个元素为 ",fruits[5])
+
+
+for key,value in ipairs(fruits)
+do
+   print(key, value)
+end
+
+
+fruits = {"banana","orange","apple","grapes"}
+print("排序前")
+for k,v in ipairs(fruits) do
+        print(k,v)
+end
+
+table.sort(fruits)
+print("排序后")
+for k,v in ipairs(fruits) do
+        print(k,v)
+end
---
+++ test.lua
@@ -0,0 +1,3 @@
+print(“中国”)
+print(“航母”)
+print(“导弹”)--  test.lua 文件末尾注释
