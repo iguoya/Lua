b6e71979c59df7122f245067c49096c161a8bb99
雪虎 <375478250@qq.com>
2021-04-07 17:15:02 +0800

    数学函数测试

---
+++ 42-math.lua
@@ -0,0 +1,27 @@
+print(math.abs(100))
+print(math.abs(-100))
+print(math.abs(-100.99))
+
+
+print(math.acos(0.5))
+
+print(math.sin(1))
+
+print(math.atan(0.5))
+print(math.atan(0.5, 0.7))
+
+print(math.ceil(0.5))
+print(math.ceil(1))
+
+
+print(math.deg(3.1415926535))
+print(math.deg(math.pi))
+
+
+print(math.ceil(3.8))
+print(math.ceil(55.3))
+print(math.ceil(6.5))
+
+print(math.floor(2.97))
+print(math.floor(5.27))
+print(math.floor(1.7))
